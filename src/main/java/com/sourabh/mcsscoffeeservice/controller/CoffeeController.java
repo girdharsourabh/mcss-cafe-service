17de20e2e91bd8485174e04f0264d721223ad83d
girdharsourabh <girdharsourabh@gmail.com>
2020-11-26 14:02:01 +0530

    Added delete

--- src/main/java/com/sourabh/mcsscoffeeservice/controller/CoffeeController.java
+++ src/main/java/com/sourabh/mcsscoffeeservice/controller/CoffeeController.java
@@ -1,6 +1,7 @@
 package com.sourabh.mcsscoffeeservice.controller;
 
 import com.sourabh.mcsscoffeeservice.model.CoffeeDTO;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +21,20 @@ public class CoffeeController {
     @PostMapping()
     public ResponseEntity saveNewCoffee(@RequestBody CoffeeDTO coffeeDTO){
         //todo
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/coffee/"+ coffeeDTO.getId());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @PutMapping("/{coffeeId}")
     public ResponseEntity updateCoffeeById(@PathVariable("coffeeId") UUID coffeeId, @RequestBody CoffeeDTO coffeeDTO){
         //todo
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{coffeeId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT )
+    public void deleteCoffee(@PathVariable("coffeeId") UUID coffeeId){
+
     }
 }
